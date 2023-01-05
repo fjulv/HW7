8381fd8a93d02e780b93ebce32fe2c55bfc70941
IULIIA <yulia918@mail.ru>
2023-01-05 22:49:02 +0700

    added all the tasks

---
+++ task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая выведет 
+// все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void EvenNumbers(int m, int n)
+{
+    if (m > n)
+    {
+        return;
+    }
+    else if (m % 2 == 0)
+    {
+        System.Console.Write($"{m}; ");
+        EvenNumbers(m + 2, n);
+    }
+    else if (m % 2 != 0)
+    {
+        m = m + 1;
+        System.Console.Write($"{m}; ");
+        EvenNumbers(m + 2, n);
+    }
+}
+
+EvenNumbers(1, 5);
+System.Console.WriteLine();
+EvenNumbers(4, 8);
+System.Console.WriteLine();
+EvenNumbers(5, 38);
---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, 
+// которая найдёт сумму натуральных элементов 
+// в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int SumNumbers(int number1, int number2)
+{
+    if (number1 == number2)
+    {
+        return number1;
+    }
+    return number1 + SumNumbers(number1 + 1, number2);
+}
+
+System.Console.WriteLine(SumNumbers(1,15));
+System.Console.WriteLine(SumNumbers(4,8));
